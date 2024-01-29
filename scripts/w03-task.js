40687994cf7ce9605dd1145e92faeea002260f46
benjaminburga <burgabenjamin872@gmail.com>
2024-01-28 19:17:14 -0500

    Update event listeners for functions

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -1,23 +1,22 @@
 /* LESSON 3 - Programming Tasks */
 
 /* FUNCTIONS */
+
 /* Function Definition - Add Numbers */
-
 function add(number1, number2) {
-    return number1 + number2;
-  }
-  
-  function addNumbers() {
-    let addNumber1 = Number(document.querySelector('#add1').value);
-    let addNumber2 = Number(document.querySelector('#add2').value);
-  
-    document.querySelector('#sum').value = add(addNumber1, addNumber2);
-  }
-  
-  document.querySelector('#addNumbers').addEventListener('click', addNumbers);
-  
-/* Function Expression - Subtract Numbers */
+  return number1 + number2;
+}
 
+function addNumbers() {
+  let addNumber1 = Number(document.querySelector('#add1').value);
+  let addNumber2 = Number(document.querySelector('#add2').value);
+
+  document.querySelector('#sum').value = add(addNumber1, addNumber2);
+}
+
+document.querySelector('#addNumbers').addEventListener('click', addNumbers);
+
+/* Function Expression - Subtract Numbers */
 const subtract = function(number1, number2) {
   return number1 - number2;
 };
@@ -28,9 +27,9 @@ const subtractNumbers = function() {
   document.querySelector('#difference').value = subtract(subtractNumber1, subtractNumber2);
 };
 
+document.querySelector('#subtractNumbers').addEventListener('click', subtractNumbers);
 
 /* Arrow Function - Multiply Numbers */
-
 const multiply = (number1, number2) => number1 * number2;
 
 const multiplyNumbers = () => {
@@ -39,9 +38,9 @@ const multiplyNumbers = () => {
   document.querySelector('#product').value = multiply(factor1, factor2);
 };
 
+document.querySelector('#multiplyNumbers').addEventListener('click', multiplyNumbers);
 
 /* Open Function Use - Divide Numbers */
-
 function divide(dividend, divisor) {
   return dividend / divisor;
 }
@@ -52,22 +51,17 @@ const divideNumbers = function() {
   document.querySelector('#quotient').value = divide(dividend, divisor);
 };
 
-
+document.querySelector('#divideNumbers').addEventListener('click', divideNumbers);
 
 /* Decision Structure */
-
 document.querySelector('#getTotal').addEventListener('click', function() {
   let subtotal = Number(document.querySelector('#subtotal').value);
   let isMember = document.querySelector('#member').checked;
-  
-  // Apply 20% discount if the user is a member
+
   let total = isMember ? subtotal * 0.8 : subtotal;
 
-  // Output the total to the 'total' span
   document.querySelector('#total').innerHTML = `$ ${total.toFixed(2)}`;
 });
-
-
 
 /* ARRAY METHODS - Functional Programming */
 let numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
