5dc373a66be02ce9a6aadaed77a06846a47d0d3d
benjaminburga <burgabenjamin872@gmail.com>
2024-02-16 00:11:36 -0500

    Adding new features to the final project

---
+++ scripts/app.js
@@ -0,0 +1,29 @@
+function getWeather() {
+    const apiKey = 'a699049b0b45b54ba8a2c400887c288d    '; 
+    const cityInput = document.getElementById('cityInput');
+    const weatherInfo = document.getElementById('weatherInfo');
+
+    const city = cityInput.value.trim();
+    if (city === '') {
+        alert('Please enter a city.');
+        return;
+    }
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+        .then(response => response.json())
+        .then(data => {
+            displayWeather(data);
+        })
+        .catch(error => {
+            console.error('Error fetching weather data:', error);
+            alert('Error fetching weather data. Please try again.');
+        });
+}
+
+function displayWeather(data) {
+    const weatherInfo = document.getElementById('weatherInfo');
+    const temperature = Math.round(data.main.temp - 273.15); // Convert from Kelvin to Celsius
+    const description = data.weather[0].description;
+
+    weatherInfo.innerHTML = `Current Weather in ${data.name}: ${temperature}°C, ${description}`;
+}
